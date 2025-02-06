31045e03e39cc9a984dd9bc276bb2655c104608c
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2025-02-06 23:26:30 +0000

    Add tests and capitalise function

---
+++ practice.js
@@ -0,0 +1,5 @@
+export function capitalise(string) {
+  const firstChar = string.slice(0, 1);
+  const newString = firstChar.toUpperCase() + string.slice(1);
+  return newString;
+}
--- practice.test.js
+++ practice.test.js
@@ -1,1 +1,6 @@
 import * as practice from "./practice.js";
+
+test("capitalises a word's first letter", () => {
+  expect(practice.capitalise("test")).toBe("Test");
+  expect(practice.capitalise("javascript")).toBe("Javascript");
+});
