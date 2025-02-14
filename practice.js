cfe7fb94aa23f41015f0967963c5a5ac7b1ab241
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2025-02-14 11:06:31 +0000

    CaesarCipher: add function implementation

--- practice.js
+++ practice.js
@@ -33,3 +33,59 @@ export const calculator = (function (firstNumber, secondNumber) {
 
   return { add, subtract, multiply, divide };
 })();
+
+function encryptCharacter(key, currentCode, lowerBound, upperBound) {
+  let newCode = currentCode + key;
+  // To allow for wrapping
+  if (newCode > upperBound) {
+    newCode = lowerBound - 1 + (newCode - upperBound);
+  }
+
+  return newCode;
+}
+
+export function caesarCipher(string, key) {
+  let encryptedString = "";
+  for (let i = 0; i < string.length; i++) {
+    const characterCode = string.charCodeAt(i);
+
+    // If character is uppercase
+    const uppercaseLowerBound = 65;
+    const uppercaseUpperBound = 90;
+    if (
+      characterCode >= uppercaseLowerBound &&
+      characterCode <= uppercaseUpperBound
+    ) {
+      const newCode = encryptCharacter(
+        key,
+        characterCode,
+        uppercaseLowerBound,
+        uppercaseUpperBound
+      );
+      encryptedString += String.fromCharCode(newCode);
+      continue;
+    }
+
+    // If character is lowercase
+    const lowercaseLowerBound = 97;
+    const lowercaseUpperBound = 122;
+    if (
+      characterCode >= lowercaseLowerBound &&
+      characterCode <= lowercaseUpperBound
+    ) {
+      const newCode = encryptCharacter(
+        key,
+        characterCode,
+        lowercaseLowerBound,
+        lowercaseUpperBound
+      );
+      encryptedString += String.fromCharCode(newCode);
+      continue;
+    }
+
+    // If no changes to be made
+    encryptedString += String.fromCharCode(characterCode);
+  }
+
+  return encryptedString;
+}
